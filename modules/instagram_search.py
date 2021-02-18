2c5e34c205ea30e840b9925b8505730dabf2e5cf
Dalunacrobate <78803631+dalunacrobate@users.noreply.github.com>
2021-02-18 02:27:15 +0100

    Update instagram_search.py

--- modules/instagram_search.py
+++ modules/instagram_search.py
@@ -2,23 +2,20 @@ import requests, bs4
 from bs4      import BeautifulSoup
 
 def ig_search(name,pren):
-    headers = {
-'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-'referrer': 'https://google.com',
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-'Accept-Encoding': 'utf-8',
-'Accept-Language': 'en-US,en;q=0.9',
-'Pragma': 'no-cache'
-}
-    r = requests.get(url='https://instajust.com/search/{} {}'.format(name,pren),headers=headers)
-    page = r.content
+    url = "https://smihub.com/search?query={}+{}".format(pren,name)
+
+    r = requests.get(url=url)
+    page = r.content.decode()
     features = "html.parser"
-    soup = BeautifulSoup(page, features)
-    profiles = soup.find_all('div',{'class':'trending_inst--inst'})
-    new_profiles = []
-    for i in profiles:
-        i = i.text
-        if pren.lower() in i.lower() and name.lower() in i.lower():
-            profile = (i.replace('\n','').replace('@',' | @'))
-            new_profiles.append(profile)
-    return new_profiles
+    soup = BeautifulSoup(page,features)
+
+    profiles = []
+
+    profiless = soup.find_all('div',{'class':'content__text'})
+    for i in profiless[0:10]:
+        i = str(i)
+        username = (i.split('</a><p>')[1].replace('</p></div>',''))
+        at_username = (i.split('</a><p>')[0].split('Instagram\'s posts" class="profile-name-link" href="')[1].split('">')[1])
+        profile_formated = ('{}\t| {}'.format(at_username,username))
+        profiles.append(profile_formated)
+    return profiles
